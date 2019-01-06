bd4ef1326df976a9c41b72a2fcd39f263300e680
Gunnar Farnebäck <gunnar@lysator.liu.se>
2019-01-06 18:10:56 +0100

    Fix typo. (#1)

--- src/intrin/avxintrin.jl
+++ src/intrin/avxintrin.jl
@@ -2,6 +2,6 @@
 const __m256d = LVec{4, Float64}
 @inline _mm256_add_ps(a::__m256d, b::__m256d) = LLVM.fadd(a, b)
 @inline function _mm256_add_ss(a::__m256d, b::__m256d)
-    v = LLVM.extractelement(a, 0) * LLVM.extractelement(b, 0)
+    v = LLVM.extractelement(a, 0) + LLVM.extractelement(b, 0)
     return LLVM.insertelement(a, v, 0)
 end
--- src/intrin/xmmintrin.jl
+++ src/intrin/xmmintrin.jl
@@ -2,6 +2,6 @@
 const __m128 = LVec{4, Float32}
 @inline _mm_add_ps(a::__m128, b::__m128) = LLVM.fadd(a, b)
 @inline function _mm_add_ss(a::__m128, b::__m128)
-    v = LLVM.extractelement(a, 0) * LLVM.extractelement(b, 0)
+    v = LLVM.extractelement(a, 0) + LLVM.extractelement(b, 0)
     return LLVM.insertelement(a, v, 0)
 end
